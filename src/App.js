77f1fcb08614df6463d0cda92e0e75153b6131bd
Richard <richard@code-labs.com>
2021-07-15 17:42:07 -0500

    Header and NavBar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './scss/main.scss';
+import { NavBar } from './components/navBar'
 
 function App() {
   return (
     <>
-      
+      <NavBar />
     </>
   );
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,9 +1,35 @@
-
+import { useState } from 'react'
+import logo from '../img/logo.svg'
+import menuMobile from '../img/menu-mobile.svg'
+import closeMenu from '../img/close.svg'
 export const NavBar = () => {
+
+    const [ hidden, setHidden ] = useState(false);
+
+    function openMenu() {
+        setHidden(true)
+    }
+    function hiddenMenu() {
+        setHidden(false)
+    }
 
     return (
         <>
-            <header> </header>
+            <header className="header"> 
+                <div className="container header__container">
+                    <img src={logo} alt="Logo Movie Test"  className='header__logo'/>
+                    <img src={menuMobile} alt="Menu Icon"  className='header__icon' onClick={openMenu} />
+                    <nav className="header__nav" hidden={!hidden}>
+                        <img src={closeMenu} alt="Close Menu Icon"  className='header__icon header__icon--close' onClick={hiddenMenu} />
+                        <ul>
+                            <li className="header__nav__item">Home</li>
+                            <li className="header__nav__item">Categories</li>
+                            <li className="header__nav__item">My Favorites</li>
+                        </ul>
+                    </nav>
+                    
+                </div>
+            </header>
         </>
     )
 }
